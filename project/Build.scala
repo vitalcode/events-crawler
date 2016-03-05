eda3cdd538959e23c86897e6b0043fe641f30951
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-03-05 23:07:36 +0000

    Update access to events model dependency

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,6 @@ import sbt._
 object MyBuild extends Build {
 
     lazy val root = Project("root", file(".")) dependsOn(eventsModelProject)
-    lazy val eventsModelProject = RootProject(uri("https://gitlab.com/vitalcode/events-model.git"))
+    lazy val eventsModelProject = RootProject(uri("https://vitalcode:vital'CHik163101@gitlab.com/vitalcode/events-model.git"))
 
 }
